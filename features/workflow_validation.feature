ebe623be6fdf022200539190842b850a9bc50524
Niko Heikkilä <niko@heikkila.io>
2025-11-30 22:26:23 +0200

    refactor: improve feature-to-test mapping

--- features/workflow_validation.feature
+++ features/workflow_validation.feature
@@ -1,6 +1,11 @@
 Feature: GitHub Actions Workflow Validation
 
+    Scenario: Complex workflow validation
+        Given a complex workflow `complex_workflow.yml`
+        When I parse the workflow
+        Then the validation should pass
+
     Scenario: Fail for using branch trigger
-        Given a workflow with a `push.branches` trigger
-        When I validate the workflow
+        Given a workflow with a `push.branches` trigger `workflow_with_push_branches.yml`
+        When I parse the workflow
         Then the validation should fail with message: "Use the `pull_request` trigger instead of the `push.branches` trigger."
--- tests/test_workflow_parsing.py
+++ tests/test_workflow_parsing.py
@@ -1,3 +1,5 @@
+"""Feature: GitHub Actions Workflow Validation"""
+
 from pathlib import Path
 
 import pytest
@@ -9,26 +11,29 @@ from ghanon.parser import parse_workflow_yaml
 
 class TestYamlParsing:
     def test_very_complex_workflow_snapshot(self):
+        """Scenario: Complex workflow validation"""
         yaml = self.load_fixture("complex_workflow.yml")
 
-        workflow = parse_workflow_yaml(yaml)
+        workflow = self.parse(yaml)
         dump = workflow.model_dump_json()
 
         assert_that(dump).snapshot("test_very_complex_workflow_snapshot")
 
     def test_workflow_with_push_branches_trigger_fails_validation(self):
-        """Test validation failure for push.branches trigger.
-
-        Feature: workflow_validation.feature
-        Scenario: Fail for using branch trigger
-        """
+        """Scenario: Fail for using branch trigger"""
         yaml = self.load_fixture("workflow_with_push_branches.yml")
 
-        with pytest.raises(
-            ValidationError,
-            match=r"Use the `pull_request` trigger instead of the `push\.branches` trigger\.",
-        ):
-            parse_workflow_yaml(yaml)
+        self.assert_failure(
+            yaml,
+            match=r"Use the `pull_request` trigger instead of the `push\.branches` trigger",
+        )
+
+    def assert_failure(self, yaml: str, match: str):
+        with pytest.raises(ValidationError, match=match):
+            self.parse(yaml)
+
+    def parse(self, yaml: str):
+        return parse_workflow_yaml(yaml)
 
     def load_fixture(self, filename: str) -> str:
         fixture_path = Path(__file__).parent / "fixtures" / filename
