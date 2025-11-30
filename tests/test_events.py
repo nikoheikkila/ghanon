a14f512dce4361e15783d2efd718b7b0012d9f16
Niko Heikkilä <niko@heikkila.io>
2025-11-30 18:02:01 +0200

    refactor: reorganise workflow run event tests

--- tests/test_events.py
+++ tests/test_events.py
@@ -36,33 +36,7 @@ from ghanon.models.workflow import (
     RegistryPackageEvent,
     ReleaseActivityType,
     ReleaseEvent,
-    WorkflowRunActivityType,
-    WorkflowRunEvent,
 )
-
-
-class TestWorkflowRunEvent:
-    def test_types(self):
-        event = WorkflowRunEvent.model_validate(
-            {
-                "types": [WorkflowRunActivityType.COMPLETED],
-                "workflows": ["CI"],
-            },
-        )
-
-        assert_that(event.types).contains(WorkflowRunActivityType.COMPLETED)
-        assert_that(event.workflows).contains("CI")
-
-    def test_branches(self):
-        event = WorkflowRunEvent.model_validate(
-            {
-                "workflows": ["Build"],
-                "branches": ["main"],
-            },
-        )
-
-        assert_that(event.branches).is_equal_to(["main"])
-        assert_that(event.workflows).contains("Build")
 
 
 class TestActivityTypeEvents:
